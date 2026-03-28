f4efda2f4da4bc7b0ca594585584acd3bda85a6a
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:22:46 +0530

    Created user model using SQLAlchemy.

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the database
+# Initialize SQLAlchemy
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-
+    password = db.Column(db.String(120), nullable=False)
+    
     def __repr__(self):
         return f'<User {self.username}>'
