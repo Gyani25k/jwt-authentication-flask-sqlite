3cba340a5325b4cd3b06c42573327e67965b80ce
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:20:15 +0530

    Smart edit

--- middleware.py
+++ middleware.py
@@ -8,3 +8,13 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+from functools import wraps
+from flask import request,jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+# Custom middleware to protect routes
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
