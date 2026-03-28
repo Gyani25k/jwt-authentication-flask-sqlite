3cf96cde04bc313fdc2ea4012bc2b14e781e9327
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:22:47 +0530

    Created schema for validation and serialization of User data.

---
+++ schemas.py
@@ -0,0 +1,6 @@
+from marshmallow import Schema, fields, validate
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True, validate=validate.Length(min=1))
+    password = fields.Str(required=True, load_only=True, validate=validate.Length(min=6))
