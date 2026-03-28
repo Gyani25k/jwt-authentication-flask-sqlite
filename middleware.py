bc5e77496dae4c00d7553a2a350566fb50e2e90d
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:21:12 +0530

    Smart edit

--- middleware.py
+++ middleware.py
@@ -18,3 +18,6 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+from functools import wraps
+from flask import request,jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
