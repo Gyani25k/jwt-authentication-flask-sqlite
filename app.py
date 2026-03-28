5dba2a29e36dd43c8c319c8fd387e5bfd6497ffc
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:24:24 +0530

    Updated imports for models and schemas in app.py.

--- app.py
+++ app.py
@@ -3,32 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 import os
 
-# Initialize the app
-app = Flask(__name__)
-
-# Configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')  # Change this in production
-
-# Initialize extensions
-db = SQLAlchemy(app)
-jwt = JWTManager(app)
-
-# Create the database
-with app.app_context():
-    db.create_all()
-
-# Import routes
-from routes import *
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-import os
-
 # Import custom modules
 from models import User
 from schemas import UserSchema
@@ -40,7 +14,7 @@ app = Flask(__name__)
 # Configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET','your_jwt_secret_key')  # Change this in production
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')  # Change this in production
 
 # Initialize extensions
 db = SQLAlchemy(app)
