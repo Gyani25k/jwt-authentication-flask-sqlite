61dd6d88e7f2fcf5edfa85b8e6309f333fa90154
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2026-03-28 23:20:56 +0530

    Update file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
@@ -16,34 +15,12 @@ app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key'
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
-# Import routes
-from routes import *
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-import os
-
-# Initialize the app
-app = Flask(__name__)
-
-# Configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')  # Change this in production
-
-# Initialize extensions
-db = SQLAlchemy(app)
-jwt = JWTManager(app)
-
-# Import routes
-from routes import *
-
-# Create database
+# Create the database
 with app.app_context():
     db.create_all()
+
+# Import routes
+from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
